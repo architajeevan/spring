8ccf0b3bd355d164a6e820dedaf026e205fdcfbd
Archita <architajeevan@gmail.com>
2026-05-09 17:26:34 +0530

    Initial Commit

--- src/main/java/com/example/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/example/spring_demo/SpringDemoApplication.java
@@ -3,11 +3,15 @@ package com.example.spring_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+		System.out.println("HEYY");
+		
 	}
 
 }
